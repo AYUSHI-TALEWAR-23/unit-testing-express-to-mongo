395b9df4479214608aa4b2bfb38c7eb548383f1f
AYUSHI-TALEWAR-23 <121218383+AYUSHI-TALEWAR-23@users.noreply.github.com>
2025-02-27 16:35:02 +0530

    Update user.test.js

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,1 +1,29 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const app = require("../app");
+const logger = require("../config/logger");
 
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+  logger.info("Connected to in-memory MongoDB for testing");
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+  logger.info("Disconnected from in-memory MongoDB after tests");
+});
+
+describe("User API Tests", () => {
+  it("should fetch users", async () => {
+    const res = await request(app).get("/api/users");
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+});
